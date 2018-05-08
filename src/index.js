16efd14532cb6b74ad41b7490c697383473ec43d
Vinod Narsing Shalgar <vinod.shalgar@oneadvanced.com>
2018-05-08 11:32:21 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyDoGuWdHCrrDTjCEmt6FdAnMnqfrjhmvkU'
 
 const App = () => {
-	return <div>vinod</div>;
+	return (<div> <SearchBar /> </div>);
 }
 
 
