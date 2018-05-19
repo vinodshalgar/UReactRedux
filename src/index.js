b137f004b31298c973db6a90fc936dce2af2e718
Vinod Narsing Shalgar <vinod.shalgar@oneadvanced.com>
2018-05-19 20:08:43 +0530

    sdf

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import YTSearch fromm 'youtube-api-search';
+
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyDoGuWdHCrrDTjCEmt6FdAnMnqfrjhmvkU'
+
+
+YTSearch({key: API_KEY , term: 'surfboards'}, function(data){
+	 console.log(data);
+});
+
+
 
 const App = () => {
 	return (
